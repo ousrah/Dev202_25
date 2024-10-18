83809b26f270168765adcbb0fa725086c9409f80
rahmouni oussama <ousrah@hotmail.com>
2024-10-18 11:19:31 +0100

    serie des triggers

--- 1 - Bases de données/1 - Mysql/1 - Cours/4 - les triggers.sql
+++ 1 - Bases de données/1 - Mysql/1 - Cours/4 - les triggers.sql
@@ -7,6 +7,8 @@ create table produit (id_produit int auto_increment primary key,
 					nom varchar(100), 
 					prix double, 
 					stock int);
+                    
+alter table produit add constraint chk_stock check (stock>=0);                    
 
 create table vente (
 				id_vente int auto_increment primary key, 
@@ -19,7 +21,7 @@ create table vente (
 insert into produit values (null,'pc',8500,15),
 							(null,'imprimance',2500,8),
                             (null,'clavier',150,25);
-                            
+# declencheur d'insertion
 drop trigger if exists insert_vente;
 delimiter $$
 create trigger insert_vente after insert on vente for each row
@@ -33,10 +35,36 @@ insert into vente values
 (null,curdate(),2,2),	# --> creation de la table system new qui a la meme structure que vente
 (null,curdate(),2,3);   # --> creation de la table system new qui a la meme structure que vente
 
+#le déclencheur et l'instruction qui l'a déclenché representent automatiquement la meme transaction
+#(soit les deux sont effectuées, soit aucune)
 
-                            
-insert into vente values (null,curdate(),1,2);    
-update produit set stock = stock-1 where id_produit = 2;                        
+insert into vente values (null,curdate(),1,2);
+        
+# declencheur de supression                         
+drop trigger if exists delete_vente;
+delimiter $$
+create trigger delete_vente after delete on vente for each row
+begin
+	update produit set stock = stock+old.qte where id_produit = old.id_produit;  
+end $$
+delimiter ;  
+        
+delete from vente where id_vente = 1; #  --> créer un table systeme nommée old qui a meme structure que vente
+
+
+# declencheur de modification
+drop trigger if exists update_vente;
+delimiter $$
+create trigger update_vente after update on vente for each row
+begin
+	update produit set stock = stock+old.qte-new.qte  where id_produit = new.id_produit;
+end $$
+delimiter ;
+
+
+
+update  vente set qte = 1 where id_vente = 2;
+update  vente set qte = 5 where id_vente = 3;
 
 select * from produit;
 select * from vente;
