4f4d5526a38c8449e325297510c4cc031346e2ec
rahmouni oussama <ousrah@hotmail.com>
2024-11-26 11:45:56 +0100

    approche agile - ms project

--- 2 - Approche agile/2 - Exercices/09. Activité 9/Activite 9_codeSource/script.sql
+++
@@ -1,9 +0,0 @@
-Create database address_book;
-
-create table
-    customers (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        name varchar(40),
-        email varchar(80),
-        mobile varchar(40)
-    );
